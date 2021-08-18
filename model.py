74e9ffc972124361c7cd5397c3422f18070bb9e6
Nardiena Pratama <nardienaalthafia@gmail.com>
2021-08-18 20:38:09 +1000

    edited model.py like tutorial

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import numpy as np
 import json
 import requests, time, logging
-from tensorflow.keras.applications import inception_v3
+from tensorflow.keras.applications import mobilenet_v2
 
 SIZE=128
 MODEL_URI = 'http://localhost:8501/v1/models/pets:predict'
@@ -23,26 +23,14 @@ def get_prediction(imagePath):
     https://www.tensorflow.org/api_docs/python/tf/keras/applications/mobilenet_v2/MobileNetV2
     
     '''
-    image = inception_v3.preprocess_input(image)
+    image = mobilenet_v2.preprocess_input(image)
     image = np.expand_dims(image, axis=0)
 
-    payload = json.dumps({
-        'instances' : image.tolist()
+    data = json.dumps({
+        'instances': image.tolist()
     })
-
-    response = requests.post(MODEL_URI, data=payload.encode('utf-8'))
+    response = requests.post(MODEL_URI, data=data.encode('utf-8'))
     result = json.loads(response.text)
-    # print("RESULT:")
-    # print(result['predictions'])
-    # prediction = np.squeeze(result['predictions'][0])
-    # print("Prediction: ", prediction)
-    # class_name = CLASSES[int(prediction > 0.5)]
-
-    # r = requests.post(MODEL_URI, json=payload)
-    # pred = json.loads(r.content.decode('utf-8'))
-    # prediction = np.squeeze(result['predictions'][0])
-    print("Prediction: ", result)
-    print(json.dumps(inception_v3.decode_predictions(np.array(result['predictions']))[0]))
-
-    
-    # return class_name
+    prediction = np.squeeze(result['predictions'][0])
+    class_name = CLASSES[int(prediction > 0.5)]
+    return class_name
