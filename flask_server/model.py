1b705ab1d02d4c1477dec3e982ea4f9814043b2d
Nardiena Pratama <nardienaalthafia@gmail.com>
2021-08-20 06:06:56 +1000

    Docker run works; docker compose still buggy

--- flask_server/model.py
+++ flask_server/model.py
@@ -1,4 +1,5 @@
 from re import DEBUG
+from flask import jsonify
 import tensorflow as tf
 import numpy as np
 import json
@@ -7,7 +8,7 @@ from tensorflow.keras.applications import mobilenet_v2
 
 SIZE=128
 MODEL_URI = 'http://localhost:8501/v1/models/pets:predict'
-# MODEL_URI = 'http://tensorflow-serving:8501/v1/models/pets:predict'
+# MODEL_URI = 'http://172.18.0.2:8501/v1/models/pets:predict'
 CLASSES = ['cat', 'dog']
 
 def get_prediction(imagePath):
@@ -36,3 +37,28 @@ def get_prediction(imagePath):
     prediction = np.squeeze(result['predictions'][0])
     class_name = CLASSES[int(prediction > 0.5)]
     return class_name
+
+    # r = requests.post(MODEL_URI, json=data)
+
+     # Decoding results from TensorFlow Serving server
+    # prediction = json.loads(r.content.decode('utf-8'))
+
+    # prediction = (np.array(prediction['predictions'])[0] > 0.5).astype(np.int)
+
+    # class_name = CLASSES[int(prediction > 0.5)]
+
+
+
+
+    # data = json.dumps({"signature_name": "serving_default", "instances":imlst})
+    # data = json.dumps({
+    #     'instances': image.tolist()
+    # })
+    # headers = {"content-type": "application/json"}
+    # json_response = requests.post(MODEL_URI, data=data, headers=headers)
+    # out = np.array((json.loads(json_response.text)['predictions']))
+
+    # # Returning JSON response
+    # return jsonify({"status": 200, "message": out.shape})
+
+    # return result
