c09f45bf972be31ea1c667b9ac7e9b859539f5ea
Naufal <nau930@gmail.com>
2021-08-22 19:54:49 +1000

    add schema validation

--- plateducate-be/app/database/schema.py
+++ plateducate-be/app/database/schema.py
@@ -32,3 +32,12 @@ user_schema = {
     "required": ["username", "email", "password"],
     "additionalProperties": False
 }
+
+def validate_user(data):
+    try:
+        validate(data, user_schema)
+    except ValidationError as e:
+        return {'ok': False, 'message': e}
+    except SchemaError as e:
+        return {'ok': False, 'message': e}
+    return {'ok': True, 'data': data}
