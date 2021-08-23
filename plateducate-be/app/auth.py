de4fe481ed753919a8dc866b4093988d695cc488
Naufal <nau930@gmail.com>
2021-08-23 23:11:25 +1000

    temp commit

--- plateducate-be/app/auth.py
+++ plateducate-be/app/auth.py
@@ -1,11 +1,37 @@
 from flask import Blueprint, jsonify, request
+from flask_bcrypt import generate_password_hash
+from . import db
+
+from database.schema import validate_user
 
 auth = Blueprint('auth', __name__)
 
 BASE_URL = "http://localhost:3000"
 
-@auth.route('/add_user', methods=['POST'])
-def add_user():
+@auth.route('/register', methods=['POST'])
+def register():
+    validate = validate_user(request.form)
+    if validate['ok']:
+        payload = validate['data']
+        print(payload)
+        hashed = generate_password_hash(payload['password']) 
+        try:
+            cursor = db.connect().cursor()
+            query = "INSERT INTO Plateducate.Users\
+                Values (%s, %s, %s, %s, %s, %s, %s)"
+            
+            # Optional fields
+            firstName = None if 'firstName' not in payload else payload['firstName']
+            lastName = None if 'lastName' not in payload else payload['lastName']
+            cursor.execute()
+            print(payload["firstNames"])
+            return jsonify({'ok': True, 'message': "success"}), 200
+        except:
+            return jsonify({'ok': False, 'message': 'Exception while inserting data'}), 200
+    else:
+        return jsonify({'ok': False,'message': "Bad request parameters: {}".format(validate['message'])}), 200
+
+
     return 'Successfully Added User', 201
 
 @auth.route('/fetch_movies', methods=['POST'])
--- plateducate-be/app/create_db_script.py
+++ plateducate-be/app/create_db_script.py
@@ -1,20 +1,21 @@
-from app import mysql
+from . import mysql
 
 cursor = mysql.connect().cursor()
 
 # cursor.execute("CREATE DATABASE Plateducate")
 
-cursor.execute(
-    "CREATE TABLE Plateducate.Users\
-    (ID int NOT NULL PRIMARY KEY, \
-    Username varchar(64) NOT NULL, \
-    Password varchar(255) NOT NULL,\
-    Email varchar(255) NOT NULL,\
-    FirstName varchar(255), \
-    LastName varchar(255), \
-    ImgSrc varchar(255))"
-    )
+# cursor.execute(
+#     "CREATE TABLE Plateducate.Users\
+#     (ID int NOT NULL PRIMARY KEY AUTO_INCREMENT, \
+#     Username varchar(64) NOT NULL, \
+#     Password varchar(255) NOT NULL,\
+#     Email varchar(255) NOT NULL,\
+#     FirstName varchar(255), \
+#     LastName varchar(255), \
+#     ImgSrc varchar(255))"
+#     )
 
 cursor.execute("SHOW DATABASES")
+
 for db in cursor:
     print(db)
