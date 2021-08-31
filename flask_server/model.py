1041584d31224bf62c4ea3fa0e79f60aed117289
Nardiena Pratama <nardienaalthafia@gmail.com>
2021-08-31 22:56:15 +1000

    Fixed bug in model.py

--- flask_server/model.py
+++ flask_server/model.py
@@ -13,12 +13,22 @@ import matplotlib
 matplotlib.use('TkAgg') 
 
 SIZE=128
-MODEL_URI = 'http://localhost:8501/v1/models/ssd_mobilenet:predict'
+model_name = 'ssd_mobilenet'
+MAX_BOXES_TO_DRAW = 3
+MODEL_URI = 'http://localhost:8501/v1/models/' + model_name + ':predict'
 # MODEL_URI = 'http://tensorflow-serving:8501/v1/models/ssd_mobilenet:predict'
 CLASSES = ['cat', 'dog']
 PATH_TO_LABELS = './label_name_100.pbtxt'
 category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS,
                                                                     use_display_name=True)
+
+def GetClassNameAndScoreList(data):
+  classes_arr = []
+  scores_arr = []
+  for cl, score in data:
+    classes_arr.append(cl['name'])
+    scores_arr.append(score)
+  return classes_arr, scores_arr
 
 # LOAD IMAGE, RUN IT THROUGH DETECTION MODEL AND VISUALIZE DETECTION RESULTS
 def load_image_into_numpy_array(path):
@@ -37,86 +47,39 @@ def load_image_into_numpy_array(path):
   return np.array(Image.open(path))
 
 def get_prediction(imagePath, fileName):
-  # image = tf.keras.preprocessing.image.load_img(imagePath, target_size=(SIZE, SIZE))
-  # image = tf.keras.preprocessing.image.img_to_array(image)
-
-  '''ssd_mobilenet
-  For MobileNetV2, call tf.keras.applications.mobilenet_v2.preprocess_input 
-  on your inputs before passing them to the model. mobilenet_v2.preprocess_input 
-  will scale input pixels between -1 and 1.
-  https://www.tensorflow.org/api_docs/python/tf/keras/applications/mobilenet_v2/MobileNetV2
-  
-  '''
-  # image = mobilenet_v2.preprocess_input(image)
-  # image = np.expand_dims(image, axis=0)
-
-  # data = json.dumps({
-  #     'instances': image.tolist()
-  # })
-  # response = requests.post(MODEL_URI, data=data.encode('utf-8'))
-  # result = json.loads(response.text)
-  # print(result)
-  # prediction = np.squeeze(result['predictions'][0])
-  # class_name = CLASSES[int(prediction > 0.5)]
-  # return class_name
-
-  # image_np = load_image_into_numpy_array(imagePath)
-  # input_tensor = tf.convert_to_tensor(image_np)
-  #   # The model expects a batch of images, so add an axis with `tf.newaxis`.
-  # input_tensor = input_tensor[tf.newaxis, ...]
-  # payload = {"instances": [image_np.tolist()]}
-  # start = time.perf_counter()
-  # res = requests.post(MODEL_URI, json=payload)
-  # print(f"Took {time.perf_counter()-start:.2f}s")
-  # detections = res.json()
-
-
-
-
-
-
   # OBJECT DETECTION MODEL
   image_np = load_image_into_numpy_array(imagePath)
-  
+  # image_np = np.expand_dims(image_np, 0)
 
   payload = {"instances": [image_np.tolist()]}
   start = time.perf_counter()
   res = requests.post(MODEL_URI, json=payload)
-  print(f"Took {time.perf_counter()-start:.2f}s")
+  end = time.perf_counter()
+  time_elapsed = end-start
+  print(f"Took {time_elapsed:.2f}s")
   detections = res.json()
 
-
+  # The JSON Response is a dictionary with key 'predictions'.
+  # We only want the value of this key which is equiv. to
+  # output of the model when ran in predict.py (from TFOD Tutorial)
   detections = detections['predictions'][0]
+  # with open('detection_boxes.txt', 'wt') as out:
+  #   pprint(detections, stream=out)
 
   # with open('output.txt', 'wt') as out:
   #   pprint(detections, stream=out)
 
   num_detections = int(detections.pop('num_detections'))
-  for key, value in detections.items():
-    print(key)
-    arrShape = np.shape(value)
-    # print(arrShape)
-
-    # IF ARRAY IS ONE-DIMENSIONAL, CONVERT TO 2D ARRAY FIRST
-    if len(arrShape) == 1:
-      value = np.reshape(value, (arrShape[0], -1))
-    else:
-      value = np.reshape(value, (arrShape[0], arrShape[1]))
-
-    # print(np.shape(value))
-    wanted_val = value[0, :num_detections]
-    if len(wanted_val) != 1:
-      wanted_val_shape = np.shape(wanted_val)
-
-      wanted_val = np.reshape(wanted_val, (1, wanted_val_shape[0]))
-    # if len(wanted_val_shape) == 1:
-    #   wanted_val = np.reshape(wanted_val, (wanted_val_shape[0], -1))
-    # else:
-    #   wanted_val = np.reshape(wanted_val, (wanted_val_shape[0], wanted_val_shape[1]))
-    print(type(wanted_val), np.shape(wanted_val))
-    detections[key] = wanted_val
   # detections = {key: value[0, :num_detections].numpy()
   #                 for key, value in detections.items()}
+
+  for key,value in detections.items():
+    # Expand dim of np arr first before slicing
+    value_expanded = np.expand_dims(value, 0)
+
+    # Do slicing
+    detections[key] = value_expanded[0, :num_detections]
+
   print('yayyy')
   detections['num_detections'] = num_detections
 
@@ -125,8 +88,6 @@ def get_prediction(imagePath, fileName):
 
   image_np_with_detections = image_np.copy()
 
-  with open('detection_boxes.txt', 'wt') as out:
-    pprint(detections, stream=out)
 
   viz_utils.visualize_boxes_and_labels_on_image_array(
         image_np_with_detections,
@@ -135,8 +96,8 @@ def get_prediction(imagePath, fileName):
         detections['detection_scores'],
         category_index,
         use_normalized_coordinates=True,
-        max_boxes_to_draw=200,
-        min_score_thresh=.30,
+        max_boxes_to_draw=MAX_BOXES_TO_DRAW,
+        min_score_thresh=.20,
         agnostic_mode=False)
 
   # fig = plt.figure()
@@ -146,16 +107,21 @@ def get_prediction(imagePath, fileName):
   new_image_path = os.path.join('static', fileName)
   im.save(new_image_path)
   # plt.show()
-  def GetClassName(data):
-   for cl in data:
-    return cl['name']
+
 
   #data processed
   classes = detections['detection_classes']
   scores = detections['detection_scores']
 
-  data = [category_index.get(value) for index,value in enumerate(classes)]
-  class_name = GetClassName(data)
-  print(GetClassName(data))
+  data = []
+  for value, score in zip(classes, scores):
+    data.append((category_index.get(value), score)) # tuple of class name with score
+    class_names, scores = GetClassNameAndScoreList(data)
 
-  return new_image_path, class_name
+
+  model_name_str = model_name.split("_")
+
+  model_name_str = " ".join(model_name_str).upper()
+
+
+  return model_name_str, new_image_path, class_names[:MAX_BOXES_TO_DRAW], scores[:MAX_BOXES_TO_DRAW], time_elapsed
