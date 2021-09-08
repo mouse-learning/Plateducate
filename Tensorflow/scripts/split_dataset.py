6c5948b9bd8fa14551033a352e0b5ea31d0b974d
Nardiena Pratama <nardienaalthafia@gmail.com>
2021-09-09 01:24:50 +1000

    Edited script to split into 3 groups: training, test, validation
    Co-authored-by: Davin Iddo Irawan Alfian <daviniddo26@gmail.com>

--- Tensorflow/scripts/split_dataset.py
+++ Tensorflow/scripts/split_dataset.py
@@ -1,3 +1,4 @@
+from genericpath import isfile
 import os
 import random
 import shutil
@@ -6,9 +7,10 @@ import shutil
 
 # 1. For every class folder, ignore .txt.
 
-dataset_path = os.walk('/media/nardiena/7C1247391246F7A2/Documents/programming-projects/food-datasets/UECFOOD256')
-TRAINING_PATH = '/media/nardiena/7C1247391246F7A2/Documents/programming-projects/food-datasets/actual-dataset/training'
-VALIDATION_PATH = '/media/nardiena/7C1247391246F7A2/Documents/programming-projects/food-datasets/actual-dataset/validation'
+dataset_path = os.walk('/media/nardiena/7C1247391246F7A22/Documents/programming-projects/plateducate-with-datasets/food-datasets/UECFOOD100')
+TRAINING_PATH = '/media/nardiena/7C1247391246F7A22/Documents/programming-projects/plateducate-with-datasets/food-datasets/actual-dataset-100/training'
+VALIDATION_PATH = '/media/nardiena/7C1247391246F7A22/Documents/programming-projects/plateducate-with-datasets/food-datasets/actual-dataset-100/validation'
+TEST_PATH = '/media/nardiena/7C1247391246F7A22/Documents/programming-projects/plateducate-with-datasets/food-datasets/actual-dataset-100/test'
 TRAINING_PERCENTAGE = 0.8
 random.seed(11)
 
@@ -35,8 +37,10 @@ for root, directories, files in dataset_path:
     assert (len(files)-1)//2 == len(fileArr)
 
     # 2b. Find the length of array of unique names. Multiply it by 80/100 (80%) -> num_of_training_data
+    # The +1 is hardcoded for this SPECIFIC dataset.
     num_of_training = round(TRAINING_PERCENTAGE * len(fileArr))
-    num_of_validation = round((1-TRAINING_PERCENTAGE) * len(fileArr))
+    num_of_validation = round((1-TRAINING_PERCENTAGE) * len(fileArr)//2)
+    num_of_test = len(fileArr) - num_of_training - num_of_validation
 
     # 3. Based on num_of_training_data (=8), take num_of_training_data (8) files from the trainingfolder randomly. The rest put into val folder.
     
@@ -45,11 +49,11 @@ for root, directories, files in dataset_path:
     random.shuffle(fileArr)
     # print("After shuffling...\n", fileArr)
 
-    
     # 3b. Pop file names
     assert num_of_training < len(fileArr)
     assert num_of_validation < len(fileArr)
-    assert num_of_training + num_of_validation == len(fileArr)
+    assert num_of_test < len(fileArr)
+    assert num_of_training + num_of_validation + num_of_test == len(fileArr)
 
     for _ in range(num_of_training):
         popped_file_name = fileArr.pop()
@@ -65,7 +69,7 @@ for root, directories, files in dataset_path:
             shutil.copy(old_path_jpg, TRAINING_PATH)        # This is for .jpg
             shutil.copy(old_path_xml, TRAINING_PATH)        # This is for .xml
 
-    assert len(fileArr) == num_of_validation
+    assert num_of_validation + num_of_test == len(fileArr) 
 
     for _ in range(num_of_validation):
         popped_file_name = fileArr.pop()
@@ -80,8 +84,31 @@ for root, directories, files in dataset_path:
         new_path_xml = os.path.join(VALIDATION_PATH, popped_file_name + '.xml')
 
         # 3c. Copy popped_file_name.jpg and popped_file_name.xml to training folder.
-        if not (os.path.isfile(new_path_jpg) or os.path.isfile(new_path_xml) or
+        if not (os.path.isfile(new_path_jpg_train) or os.path.isfile(new_path_xml_train) or
             os.path.isfile(new_path_jpg) or os.path.isfile(new_path_xml)):
 
             shutil.copy(old_path_jpg, VALIDATION_PATH)        # This is for .jpg
             shutil.copy(old_path_xml, VALIDATION_PATH)        # This is for .xml
+
+    for _ in range(num_of_test):
+        popped_file_name = fileArr.pop()
+        
+        old_path_jpg = os.path.join(root, popped_file_name + '.jpg')
+        old_path_xml = os.path.join(root, popped_file_name + '.xml')
+
+        new_path_jpg_train = os.path.join(TRAINING_PATH, popped_file_name + '.jpg')
+        new_path_xml_train = os.path.join(TRAINING_PATH, popped_file_name + '.xml')
+
+        new_path_jpg_validation = os.path.join(VALIDATION_PATH, popped_file_name + '.jpg')
+        new_path_xml_validation = os.path.join(VALIDATION_PATH, popped_file_name + '.xml')
+
+        new_path_jpg = os.path.join(TEST_PATH, popped_file_name + '.jpg')
+        new_path_xml = os.path.join(TEST_PATH, popped_file_name + '.xml')
+
+        # 3c. Copy popped_file_name.jpg and popped_file_name.xml to training folder.
+        if not (os.path.isfile(new_path_jpg_train) or os.path.isfile(new_path_xml_train) or
+            os.path.isfile(new_path_jpg_validation) or os.path.isfile(new_path_xml_validation) or
+            os.path.isfile(new_path_jpg) or os.path.isfile(new_path_xml)):
+
+            shutil.copy(old_path_jpg, TEST_PATH)        # This is for .jpg
+            shutil.copy(old_path_xml, TEST_PATH)        # This is for .xml
