75ff975fcfb7a3ef4294293c16eeb4ed6e2504e0
Davin Iddo Irawan Alfian <daviniddo26@gmail.com>
2021-09-23 23:04:55 +1000

    Send the image to the ML part

--- plateducate-be/app/auth.py
+++ plateducate-be/app/auth.py
@@ -65,14 +65,14 @@ def login():
 @auth.route('/submit_photo', methods=['POST', 'GET'])
 def submit_photo():
     base64Image = request.data
-    # url = 'http://localhost:5000/receive_photo'
-    # response = requests.post(url, data=base64Image)
+    url = 'http://localhost:5000/receive_photo'
+    response = requests.post(url, data=base64Image)
 
-    dataType = str(type(base64Image))
-    dataContent = str(base64Image)
-    return jsonify({'dataType': dataType, 'message': "image post request sent", 'ok': True, 'dataContent': dataContent}), 200
+    # dataType = str(type(base64Image))
+    # dataContent = str(base64Image)
+    # return jsonify({'dataType': dataType, 'message': "image post request sent", 'ok': True, 'dataContent': dataContent}), 200
 
-    # return (response.text, response.status_code, response.headers.items())
+    return (response.text, response.status_code, response.headers.items())
 
 @auth.route('/logout')
 def logout():
