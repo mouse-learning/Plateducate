d4c1a5a7b1f97d39de6b08547b0250ac02a924e0
Nardiena Pratama <nardienaalthafia@gmail.com>
2021-09-29 22:44:10 +1000

    [GREEN] - yolo model implemented with tf serving and darkflow; can predict successfully with bounding boxes, though still on compressed image

--- plateducate-ml/flask_server/app.py
+++ plateducate-ml/flask_server/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for, request, redirect, jsonify
 from flask_bootstrap import Bootstrap
+from PIL import Image
+import base64
+import io
 
 import os
 import model, time
@@ -20,15 +23,15 @@ def index():
             # print(image_path)
             uploaded_file.save(image_path)
             # print("Uploaded")
-            model_name, image_bb_path, class_name, scores, time_elapsed = model.get_prediction(image_path, uploaded_file.filename, 'ssd_mobilenet')
-            resultMobile = {
-                'model_name': model_name,
-                'class_with_scores': zip(class_name, scores),
-                'image_path': image_bb_path, 
-                'time_elapsed': time_elapsed
-            }
+            # model_name, image_bb_path, class_name, scores, time_elapsed = model.get_prediction(image_path, uploaded_file.filename, 'ssd_mobilenet')
+            # resultMobile = {
+            #     'model_name': model_name,
+            #     'class_with_scores': zip(class_name, scores),
+            #     'image_path': image_bb_path, 
+            #     'time_elapsed': time_elapsed
+            # }
 
-            model_name, image_bb_path, class_name, scores, time_elapsed = model.get_prediction(image_path, uploaded_file.filename, 'ssd_resnet101')
+            model_name, image_bb_path, class_name, scores, time_elapsed = model.get_prediction_yolo(image_path, uploaded_file.filename, 'yolo-tf1')
             resultResnet = {
                 'model_name': model_name,
                 'class_with_scores': zip(class_name, scores),
@@ -36,19 +39,57 @@ def index():
                 'time_elapsed': time_elapsed
             }
 
-            return render_template('result.html', resultMobile = resultMobile, resultResnet = resultResnet)
+            return render_template('result.html', resultResnet = resultResnet)
     return render_template('index.html')
 
 @app.route('/receive_photo', methods=['POST', 'GET'])
 def receive_photo():
     data = request.data
-    
-    dataIsBytes = True if type(data) == bytes else False
+    decoded = base64.b64decode(data)
+    image = Image.open(io.BytesIO(decoded))
 
-    if data:
-        return jsonify({'ok': True, 'message': "ML flask server - image post request retrieved", 'typeIsBytes': dataIsBytes}), 200
+    # dataType = str(type(data))
+    # dataContent = str(data)
+    # return jsonify({'ok': True, 'message': "ML flask server - image post request retrieved", 'data type': dataType, 'data content': dataContent}), 200
+
+    # time.sleep(2)
+    model_name, class_name, scores, time_elapsed = model.get_prediction_v2(image, 'ssd_mobilenet')
+    resultMobile = {
+        'model_name': model_name,
+        'class_with_scores': {
+            'class_name': class_name,
+            'scores': scores
+        },
+        'time_elapsed': time_elapsed
+    }
+
+    model_name, class_name, scores, time_elapsed = model.get_prediction_v2(image, 'ssd_resnet101')
+    resultResnet = {
+        'model_name': model_name,
+        # 'class_with_scores': zip(class_name, scores),
+        'class_with_scores': {
+            'class_name': class_name,
+            'scores': scores
+        },
+        'time_elapsed': time_elapsed
+    }
+
+    if (resultResnet and resultMobile):
+    # if (resultMobile):
+    # if (resultResnet):
+        return jsonify({'ok': True, 'message': "ML prediction result", 'resultMobile': resultMobile, 'resultResnet': resultResnet}), 200
+        # return jsonify({'ok': True, 'message': "ML prediction result", 'resultMobile': resultMobile}), 200
+        # return jsonify({'ok': True, 'message': "ML prediction result", 'resultMobile': resultResnet}), 200
     else:
-        return jsonify({'ok': False, 'message': "ML flask server - no data found", 'typeIsBytes': dataIsBytes}), 400
+        return jsonify({'ok': False, 'message': "ML flask server - no data found"}), 400
+
+    # dataType = type(resultMobile)
+    # if data:
+    #     return jsonify({'ok': True, 'message': "ML flask server - image post request retrieved", 'dataType': dataType}), 200
+    # else:
+    #     return jsonify({'ok': False, 'message': "ML flask server - no data found", 'dataType': dataType}), 400
+
+
 
 if __name__ == '__main__':
     # app.run(debug = True)
