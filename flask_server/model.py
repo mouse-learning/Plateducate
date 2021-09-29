c4ab0a3fac5a77e8a0dd5f967e33bce11edf8bcd
Nardiena Pratama <nardienaalthafia@gmail.com>
2021-09-29 22:44:10 +1000

    [GREEN] - yolo model implemented with tf serving and darkflow; can predict successfully with bounding boxes, though still on compressed image

--- flask_server/model.py
+++ flask_server/model.py
@@ -1,20 +1,20 @@
 from re import DEBUG
 from flask import jsonify
 import numpy as np
-import os
+import os, json, cv2
 import requests, time, logging
-from object_detection.utils import visualization_utils as viz_utils
-from object_detection.utils import label_map_util
+# from object_detection.utils import visualization_utils as viz_utils
+# from object_detection.utils import label_map_util
+from darkflow.net.build import TFNet
 from PIL import Image
-import io
-import base64
+from pprint import pprint
 
 SIZE=128
 MAX_BOXES_TO_DRAW = 1
 MIN_SCORE_THRESH = 0.3
-PATH_TO_LABELS = './label_name_100.pbtxt'
-category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS,
-                                                                    use_display_name=True)
+# PATH_TO_LABELS = './label_name_100.pbtxt'/media/nardiena/7C1247391246F7A22/Documents/programming-projects/plateducate-with-datasets/Plateducate/plateducate-ml/flask_server
+# category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS,
+#                                                                     use_display_name=True)
 
 def convertLabelToStr(data):
   model_name_str = data.split("_")
@@ -50,6 +50,110 @@ def load_image_into_numpy_array(path):
   """
   return np.array(Image.open(path))
 
+
+
+def get_prediction_yolo(imagePath, fileName, modelName):
+  MODEL_URI = 'http://localhost:8501/v1/models/' + modelName + ':predict'
+  # MODEL_URI = 'http://tensorflow-serving:8501/v1/models/' + modelName + ':predict'
+
+
+  options = {"model": "darkflow/cfg/yolov2-food100.cfg", "load": "darkflow/weights/yolov2-food100_10000.weights", "labels": "darkflow/labels.txt", "threshold": 0.1}
+  tfnet = TFNet(options)
+
+
+  # OBJECT DETECTION MODEL
+  im = cv2.imread(imagePath)
+  img_shape = im.shape[:2]
+  # print(img_shape)
+  # cv2.imshow("lalala", im)
+  # cv2.waitKey(0)
+  # cv2.destroyAllWindows()
+  imsz = cv2.resize(im, (416, 416))
+  imsz = imsz / 255.
+  imsz = imsz[:, :, ::-1]
+  reshaped_img_shape = imsz.shape[:2]
+  scale = np.flipud(np.divide(reshaped_img_shape, img_shape))  # you have to flip because the image.shape is (y,x) but your corner points are (x,y)
+  # print(scale)
+
+
+  payload = '{"signature_name":"predict", "instances" : [{"input": %s}]}' % imsz.tolist()
+  start = time.perf_counter()
+  res = requests.post(MODEL_URI, data=payload)
+  end = time.perf_counter()
+  time_elapsed = end-start
+  print(f"Took {time_elapsed:.2f}s")
+  time_elapsed = round(time_elapsed, 2)
+
+  json_response = json.loads(res.text)
+  with open('res.txt', 'wt') as out:
+    pprint(json_response, stream=out)
+  net_out = np.squeeze(np.array(json_response['predictions'], dtype='float32'))
+  # with open('net.txt', 'wt') as out:
+  #   pprint(net_out, stream=out)
+  detections = res.json()
+
+  # The JSON Response is a dictionary with key 'predictions'.
+  # We only want the value of this key which is equiv. to
+  # output of the model when ran in predict.py (from TFOD Tutorial)
+  detections = detections['predictions'][0]
+
+  boxes = tfnet.framework.findboxes(net_out)
+  h, w, _ = imsz.shape
+  threshold = tfnet.FLAGS.threshold
+  boxesInfo = list()
+  for box in boxes:
+      tmpBox = tfnet.framework.process_box(box, h, w, threshold)
+      if tmpBox is None:
+          continue
+      boxesInfo.append({
+          "label": tmpBox[4],
+          "confidence": tmpBox[6],
+          "topleft": {
+              "x": tmpBox[0],
+              "y": tmpBox[2]},
+          "bottomright": {
+              "x": tmpBox[1],
+              "y": tmpBox[3]}
+      })
+
+  class_names_w_scores = []
+  new_image_path = os.path.join('static', modelName + fileName)
+  imrsz = cv2.resize(im, (416, 416))
+  imrsz = imrsz / 255.
+  # imrsz = imrsz[:, :, ::-1]
+  for prediction in boxesInfo:
+      # print(prediction)
+      old_top_left = np.array([prediction['topleft']['x'], prediction['topleft']['y']])
+      old_bottom_right = np.array([prediction['bottomright']['x'], prediction['bottomright']['y']])
+
+      new_top_left_corner = np.multiply(old_top_left, scale)
+      new_bottom_right_corner = np.multiply(old_bottom_right, scale )
+      # print("New top left: ", new_top_left_corner)
+      # print("New bottom right:", new_bottom_right_corner)
+
+      cv2.rectangle(imrsz, (int(old_top_left[0]), int(old_top_left[1])), (int(old_bottom_right[0]), int(old_bottom_right[1])), (255,0,0))
+      new_img = cv2.convertScaleAbs(imrsz, alpha=(255.0))
+      new_img = cv2.resize(new_img, img_shape)
+      cv2.imwrite(new_image_path,new_img)
+      # cv2.imshow("lalala", new_img)
+      # cv2.waitKey(0)
+      # cv2.destroyAllWindows()
+      class_names_w_scores.append((prediction['label'], prediction['confidence']))
+      # {'label': 'bibimbap', 'confidence': 0.80018985, 'topleft': {'x': 67, 'y': 5}, 'bottomright': {'x': 415, 'y': 409}}
+
+  class_names_w_scores = np.array(class_names_w_scores)
+  class_names = class_names_w_scores[:, 0]
+  scores = class_names_w_scores[:, 1]
+
+  model_name_str = convertLabelToStr(modelName)
+
+  
+
+
+
+
+  return model_name_str, new_image_path, class_names, scores, time_elapsed
+
 def get_prediction(imagePath, fileName, modelName):
   MODEL_URI = 'http://localhost:8501/v1/models/' + modelName + ':predict'
   # MODEL_URI = 'http://tensorflow-serving:8501/v1/models/' + modelName + ':predict'
@@ -66,7 +170,10 @@ def get_prediction(imagePath, fileName, modelName):
   time_elapsed = end-start
   print(f"Took {time_elapsed:.2f}s")
   time_elapsed = round(time_elapsed, 2)
+
   detections = res.json()
+  with open('res.txt', 'wt') as out:
+    pprint(detections, stream=out)
 
   # The JSON Response is a dictionary with key 'predictions'.
   # We only want the value of this key which is equiv. to
@@ -154,7 +261,7 @@ def load_image_into_numpy_array_v2(img):
 
 def get_prediction_v2(imageFile, modelName):
   MODEL_URI = 'http://localhost:8501/v1/models/' + modelName + ':predict'
-  # MODEL_URI = 'http://tensorflow-serving:8501/v1/models/ssd_mobilenet:predict'
+  # MODEL_URI = 'http://tensorflow-serving:8501/v1/models/' + modelName+ ':predict'
 
 
   # OBJECT DETECTION MODEL
