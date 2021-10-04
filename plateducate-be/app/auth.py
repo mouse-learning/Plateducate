9a77a6228c6e4851769a44dfe2591ecf75b3210e
Davin Iddo Irawan Alfian <daviniddo26@gmail.com>
2021-10-04 21:13:39 +1000

    Moved the endpoint into a more appropriate file

--- plateducate-be/app/auth.py
+++ plateducate-be/app/auth.py
@@ -66,22 +66,6 @@ def login():
 
     return jsonify({'ok': True, 'message': "Success login"}), 200
 
-<<<<<<< HEAD
-=======
-# Temporary route - change to other file later!
-@auth.route('/submit_photo', methods=['POST', 'GET'])
-def submit_photo():
-    base64Image = request.data
-    url = 'http://localhost:5000/receive_photo'
-    response = requests.post(url, data=base64Image)
-
-    # dataType = str(type(base64Image))
-    # dataContent = str(base64Image)
-    # return jsonify({'dataType': dataType, 'message': "image post request sent", 'ok': True, 'dataContent': dataContent}), 200
-
-    return (response.text, response.status_code, response.headers.items())
-
->>>>>>> 07c28a6 (add route to retrieve post request of base64 image)
 @auth.route('/logout')
 def logout():
     # reset the current USER_ID
