7bffc5a609a93b9b852a6c83caf0a1206324241e
Davin Iddo Irawan Alfian <daviniddo26@gmail.com>
2021-10-04 21:13:39 +1000

    Moved the endpoint into a more appropriate file

--- plateducate-be/app/auth.py
+++ plateducate-be/app/auth.py
@@ -37,7 +37,7 @@ def register():
         # except:
         #     return jsonify({'ok': False, 'message': "Exception found committing to database"}), 500
 
-    return jsonify({'ok': False, 'message': "False request method"}), 400
+    return jsonify({'ok': False, 'message': "False request method"}), 200
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -68,19 +68,6 @@ def login():
                     return jsonify({'ok': False, 'message': "Incorrect password"}), 400
 
     return jsonify({'ok': True, 'message': "Success login"}), 200
-
-# Temporary route - change to other file later!
-@auth.route('/submit_photo', methods=['POST', 'GET'])
-def submit_photo():
-    base64Image = request.data
-    url = 'http://localhost:5000/receive_photo'
-    response = requests.post(url, data=base64Image)
-
-    # dataType = str(type(base64Image))
-    # dataContent = str(base64Image)
-    # return jsonify({'dataType': dataType, 'message': "image post request sent", 'ok': True, 'dataContent': dataContent}), 200
-
-    return (response.text, response.status_code, response.headers.items())
 
 @auth.route('/logout')
 def logout():
