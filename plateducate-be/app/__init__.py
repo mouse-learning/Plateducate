40eb7805b8e0be78fc80ba1227eb743376b39068
Davin Iddo Irawan Alfian <daviniddo26@gmail.com>
2021-10-04 21:18:11 +1000

    Modified to accomodate the new index.py file

--- plateducate-be/app/__init__.py
+++ plateducate-be/app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, request, redirect
 
 import os
 from .auth import auth
+from .objectDetection import objectDetection
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    app.register_blueprint(auth)
+app.register_blueprint(auth)
+app.register_blueprint(objectDetection)
 
-    return app
+if __name__ == '__main__':
+    app.run()
