5eafcdc879a259a4398ad90e7d99d1242117ec77
Muhammad Naufal <nau930@gmail.com>
2021-10-07 14:29:36 +1000

    establish API calls from FE to BE

--- plateducateFE/App.js
+++ plateducateFE/App.js
@@ -1,112 +1,89 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import * as React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+import LoginScreen from './Screen/LoginScreen'; 
+import RegisterScreen from './Screen/RegisterScreen';
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    landing: {
+        color: 'rgb(59,108,212)',
+        fontSize: 42,
+        fontWeight: '100',
+        textAlign: 'center',
+    },
+    login: { 
+        flex: 1, 
+        alignItems: 'center', 
+        justifyContent: 'center' 
+    }
+})
+
+const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
+
+function SplashPage({navigation}) {
+    return (
+        // <View style={styles.container}>
+        //     <Text style={styles.landing}>Plateducate</Text>
+        //     <Button 
+        //         title="Login" 
+        //         onPress={() => navigation.navigate('Login')}
+        //         />
+            <Tab.Navigator>
+                <Tab.Screen name="Register Page" component={Register} />
+                <Tab.Screen name="Login Page" component={Login} />
+            </Tab.Navigator>
+        // </View>
+    )
+}
+
+function Register() {
+    return (
+        <View style={styles.login}>
+            <Text>Register Screen</Text>
+        </View>
+    );
+}
+
+function Login() {
+    return (
+        <View style={styles.login}>
+            <Text>Login Screen</Text>
+        </View>
+    );
+}
+
+function Profile() {
+    return (
+        <View style={styles.login}>
+            <Text>Profile Screen</Text>
+        </View>
+    );
+}
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Landing">
+                {/* <Stack.Screen 
+                    name="Landing" 
+                    component={SplashPage} 
+                    options={{headerShown: false}}    
+                />
+                <Stack.Screen name="Profile" component={Profile} /> */}
+                <Stack.Screen name="Register" component={RegisterScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
 
 export default App;
