3b34f0585c92578193c28acffe833970008c412c
Nardiena Pratama <nardienaalthafia@gmail.com>
2021-10-17 14:32:30 +1000

    Create MyDiet tab button

--- plateducateFE/App.js
+++ plateducateFE/App.js
@@ -1,6 +1,7 @@
 
 // Import React and Component
 import React from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import LoginScreen from './Screen/LoginScreen'; 
 import RegisterScreen from './Screen/RegisterScreen';
 import SplashScreen from './Screen/SplashScreen';
@@ -8,13 +9,15 @@ import HomeScreen from './Screen/HomeScreen';
 import MyDietScreen from './Screen/MyDietScreen';
 import RecommendationScreen from './Screen/RecommendationScreen';
 
+
 // Import Navigators from React Navigation
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Stack = createNativeStackNavigator()
-const Tab = createBottomTabNavigator()
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
 
 const Auth = () => {
   // Stack Navigator for Login and Sign up Screen
@@ -47,8 +50,8 @@ const HomeStack = createNativeStackNavigator();
 function HomeStackScreen() {
     return (
         <HomeStack.Navigator>
-            <HomeStack.Screen name="Homepage" component={HomeScreen} />
-            <HomeStack.Screen name="MyDiet" component={MyDietScreen}/>
+            <HomeStack.Screen name="Homepage" component={HomeScreen} options={{headerShown: false }}/>
+            <HomeStack.Screen name="MyDiet" component={MyDietScreen}  options={{headerShown: false }}/>
             <HomeStack.Screen name="Recommendations" component={RecommendationScreen}/>
         </HomeStack.Navigator>
     )
@@ -56,9 +59,33 @@ function HomeStackScreen() {
 
 function LoggedIn() {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === 'Home') {
+                iconName = focused
+                  ? 'home'
+                  : 'home-outline';
+              } else if (route.name === 'My Diet') {
+                iconName = focused ? 'calendar' : 'calendar-outline';
+              }
+              else if (route.name === 'Profile') {
+                iconName = focused ? 'person-circle' : 'person-circle-outline';
+              }
+
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: '#f5c92c',
+            tabBarInactiveTintColor: 'gray',
+          })}
+          >
             {/* List of tabs at bottom of screen */}
             <Tab.Screen name="Home" component={HomeStackScreen} options={{headerShown: false}}/>
+            <Tab.Screen name="My Diet" component={MyDietScreen} options={{headerShown: false}}/>
+            <Tab.Screen name="Profile" component={RecommendationScreen} options={{headerShown: false}}/>
             {/* <Tab.Screen name="Profile"/> */}
         </Tab.Navigator>
     )
