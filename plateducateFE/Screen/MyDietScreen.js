2dffc3e2ff499968eb49fcc454f2da16c2c30b2f
Nardiena Pratama <nardienaalthafia@gmail.com>
2021-10-17 14:30:12 +1000

    Create floating action button in diet page

--- plateducateFE/Screen/MyDietScreen.js
+++ plateducateFE/Screen/MyDietScreen.js
@@ -1,48 +1,125 @@
-// Example of Splash, Login and Sign Up in React Native
-// https://aboutreact.com/react-native-login-and-signup/
+// REFERENCE: https://www.positronx.io/react-native-pick-images-from-camera-gallery-example/
 
-// Import React and Component
-import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import React, { Component, useState, useCallback } from 'react';
+import {
+  View, 
+  Text, 
+  SafeAreaView, 
+  StyleSheet,
+  TouchableOpacity, 
+  Button,
+  Image
+} from 'react-native';
+import { launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import { FloatingAction } from "react-native-floating-action";
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const MyDietScreen = () => {
+
+export default function MyDietScreen() {
+  const actions = [
+    {
+      text: "Add from camera",
+      icon: require("../static/camera.png"),
+      name: "camera-btn",
+      position: 1,
+    },
+    {
+      text: "Add from gallery",
+      icon: require("../static/gallery.png"),
+      name: "gallery-btn",
+      position: 2
+    },
+  ];
+
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <View style={{flex: 1, padding: 16}}>
-        <View
-          style={{
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Text
-            style={{
-              fontSize: 20,
-              textAlign: 'center',
-              marginBottom: 16,
-            }}>
-            This is the My Diet Screen
-          </Text>
-        </View>
-        <Text
-          style={{
-            fontSize: 18,
-            textAlign: 'center',
-            color: 'grey',
-          }}>
-          Splash, Login and Register Example{'\n'}React Native
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.titleStyle}>
+          Example of Floating Action Button
+          with Multiple Option in React Native
         </Text>
-        <Text
-          style={{
-            fontSize: 16,
-            textAlign: 'center',
-            color: 'grey',
-          }}>
-          www.aboutreact.com
+        <Text style={styles.textStyle}>
+          Click on Action Button to see Alert
         </Text>
+        <FloatingAction
+          // ref={(ref) => { this.floatingAction = ref; }}
+          actions={actions}
+          onPressItem={name => {
+            if (name == 'camera-btn') {
+              onCameraPress();
+            } else if (name == 'gallery-btn') {
+              onImageLibraryPress();
+            }
+            console.log(`selected button: ${name}`);
+          }}
+        />
+        
       </View>
     </SafeAreaView>
   );
 };
 
-export default MyDietScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 10,
+  },
+  titleStyle: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 10,
+  },
+  textStyle: {
+    fontSize: 16,
+    textAlign: 'center',
+    padding: 10,
+  },
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
+  },
+});
+// const MyDietScreen = () => {
+//   return (
+//     <SafeAreaView style={{flex: 1}}>
+//       <View style={{flex: 1, padding: 16}}>
+//         <View
+//           style={{
+//             flex: 1,
+//             alignItems: 'center',
+//             justifyContent: 'center',
+//           }}>
+//           <Text
+//             style={{
+//               fontSize: 20,
+//               textAlign: 'center',
+//               marginBottom: 16,
+//             }}>
+//             This is the My Diet Screen
+//           </Text>
+//         </View>
+//         <Text
+//           style={{
+//             fontSize: 18,
+//             textAlign: 'center',
+//             color: 'grey',
+//           }}>
+//           Splash, Login and Register Example{'\n'}React Native
+//         </Text>
+//         <Text
+//           style={{
+//             fontSize: 16,
+//             textAlign: 'center',
+//             color: 'grey',
+//           }}>
+//           www.aboutreact.com
+//         </Text>
+//       </View>
+//     </SafeAreaView>
+//   );
+// };
+
+// export default MyDietScreen;
