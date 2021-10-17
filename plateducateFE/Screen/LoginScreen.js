5f6d64db56c3d20895cf8be4e48e5615fcd3f6b6
Muhammad Naufal <nau930@gmail.com>
2021-10-17 13:10:17 +1000

    direct user to homepage on login

--- plateducateFE/Screen/LoginScreen.js
+++ plateducateFE/Screen/LoginScreen.js
@@ -16,6 +16,139 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import Loader from './Components/Loader';
 
+const LoginScreen = ({ navigation }) => {
+   const [username, setUsername] = useState('');
+   const [userPassword, setUserPassword] = useState('');
+   const [loading, setLoading] = useState(false);
+   const [errortext, setErrortext] = useState('');
+
+   const passwordInputRef = createRef();
+   const usernameInputRef = createRef();
+
+   const handleSubmitBUtton = () => {
+      setErrortext('');
+      if (!username) {
+        alert('Please fill Username');
+        return;
+      }
+      if (!userPassword) {
+        alert('Please fill Password');
+        return;
+      }
+      setLoading(true);
+      
+      var dataToSend = {
+         username: username,
+         password: userPassword,
+       };
+       console.log(dataToSend)
+
+      fetch('http://10.0.2.2:5000/login', {
+        method: 'POST',
+        body: JSON.stringify(dataToSend),
+        headers: {
+         Accept: 'application/json',
+         'Content-Type': 'application/json',
+       },
+      })
+        .then((response) => response.json())
+        .then((responseJson) => {
+           if (responseJson.ok == true) {
+              setLoading(false);
+              AsyncStorage.setItem('@user_id', responseJson.access_token);
+              AsyncStorage.setItem('@username', dataToSend['username']);
+              console.log()
+              navigation.replace('LoggedIn');
+            } else {
+               setLoading(false);
+               alert(responseJson.message);
+          }
+        })
+        .catch((error) => {
+          setLoading(false);
+          console.error(error);
+        });
+    };
+
+   return (
+      <View style={styles.mainBody}>
+         <Loader loading={loading} />
+         <ScrollView
+            keyboardShouldPersistTaps="handled"
+            contentContainerStyle={{
+               flex: 1,
+               justifyContent: 'center',
+               alignContent: 'center',
+            }}>
+            <View>
+               <KeyboardAvoidingView enabled>
+                  <View style={{ alignItems: 'center' }}>
+                     <Image
+                        source={require('../static/logo.png')}
+                        style={{
+                           width: '50%',
+                           height: 100,
+                           resizeMode: 'contain',
+                           margin: 30,
+                        }}
+                     />
+                  </View>
+                  <View style={styles.SectionStyle}>
+                     <TextInput
+                        style={styles.inputStyle}
+                        onChangeText={(username) => setUsername(username)}
+                        placeholder="Enter Username"
+                        placeholderTextColor="#8b9cb5"
+                        autoCapitalize="none"
+                        keyboardType="default"
+                        returnKeyType="next"
+                        ref={usernameInputRef}
+                        onSubmitEditing={() =>
+                           usernameInputRef.current && usernameInputRef.current.focus()
+                        }
+                        underlineColorAndroid="#f000"
+                        blurOnSubmit={false}
+                     />
+                  </View>
+                  <View style={styles.SectionStyle}>
+                     <TextInput
+                        style={styles.inputStyle}
+                        onChangeText={(UserPassword) => setUserPassword(UserPassword)}
+                        placeholder="Enter Password"
+                        placeholderTextColor="#8b9cb5"
+                        keyboardType="default"
+                        ref={passwordInputRef}
+                        onSubmitEditing={() =>
+                           passwordInputRef.current && passwordInputRef.current.focus()
+                        }
+                        blurOnSubmit={false}
+                        secureTextEntry={true}
+                        underlineColorAndroid="#f000"
+                        returnKeyType="next"
+                     />
+                  </View>
+                  {errortext != '' ? (
+                     <Text style={styles.errorTextStyle}> {errortext} </Text>
+                  ) : null}
+                  <TouchableOpacity
+                     style={styles.buttonStyle}
+                     activeOpacity={0.5}
+                     onPress={handleSubmitBUtton}
+                  >
+                     <Text style={styles.buttonTextStyle}>LOGIN</Text>
+                  </TouchableOpacity>
+                  <Text
+                     style={styles.registerTextStyle}
+                     onPress={() => navigation.navigate('RegisterScreen')}>
+                     New Here ? Register
+                  </Text>
+               </KeyboardAvoidingView>
+            </View>
+         </ScrollView>
+      </View>
+   );
+};
+
 const styles = StyleSheet.create({
    mainBody: {
       flex: 1,
@@ -72,90 +205,5 @@ const styles = StyleSheet.create({
       fontSize: 14,
    },
 });
-
-const LoginScreen = ({ navigation }) => {
-   const [userEmail, setUserEmail] = useState('');
-   const [userPassword, setUserPassword] = useState('');
-   const [loading, setLoading] = useState(false);
-   const [errortext, setErrortext] = useState('');
-
-   const passwordInputRef = createRef();
-
-   return (
-      <View style={styles.mainBody}>
-         <Loader loading={loading} />
-         <ScrollView
-            keyboardShouldPersistTaps="handled"
-            contentContainerStyle={{
-               flex: 1,
-               justifyContent: 'center',
-               alignContent: 'center',
-            }}>
-            <View>
-               <KeyboardAvoidingView enabled>
-                  <View style={{ alignItems: 'center' }}>
-                     <Image
-                        source={require('../static/logo.png')}
-                        style={{
-                           width: '50%',
-                           height: 100,
-                           resizeMode: 'contain',
-                           margin: 30,
-                        }}
-                     />
-                  </View>
-                  <View style={styles.SectionStyle}>
-                     <TextInput
-                        style={styles.inputStyle}
-                        onChangeText={(UserEmail) => setUserEmail(UserEmail)}
-                        placeholder="Enter Email" //dummy@abc.com
-                        placeholderTextColor="#8b9cb5"
-                        autoCapitalize="none"
-                        keyboardType="email-address"
-                        returnKeyType="next"
-                        onSubmitEditing={() =>
-                           passwordInputRef.current && passwordInputRef.current.focus()
-                        }
-                        underlineColorAndroid="#f000"
-                        blurOnSubmit={false}
-                     />
-                  </View>
-                  <View style={styles.SectionStyle}>
-                     <TextInput
-                        style={styles.inputStyle}
-                        onChangeText={(UserPassword) => setUserPassword(UserPassword)}
-                        placeholder="Enter Password" //12345
-                        placeholderTextColor="#8b9cb5"
-                        keyboardType="default"
-                        ref={passwordInputRef}
-                        onSubmitEditing={Keyboard.dismiss}
-                        blurOnSubmit={false}
-                        secureTextEntry={true}
-                        underlineColorAndroid="#f000"
-                        returnKeyType="next"
-                     />
-                  </View>
-                  {errortext != '' ? (
-                     <Text style={styles.errorTextStyle}> {errortext} </Text>
-                  ) : null}
-                  <TouchableOpacity
-                     style={styles.buttonStyle}
-                     activeOpacity={0.5}
-                     onPress={() => navigation.navigate('Home')}
-                  // onPress={handleSubmitPress}
-                  >
-                     <Text style={styles.buttonTextStyle}>LOGIN</Text>
-                  </TouchableOpacity>
-                  <Text
-                     style={styles.registerTextStyle}
-                     onPress={() => navigation.navigate('RegisterScreen')}>
-                     New Here ? Register
-                  </Text>
-               </KeyboardAvoidingView>
-            </View>
-         </ScrollView>
-      </View>
-   );
-};
 
 export default LoginScreen;
