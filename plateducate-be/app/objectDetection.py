40abc8fc5ae394c946290022b2613e98cb27bb0a
Nardiena Pratama <nardienaalthafia@gmail.com>
2021-10-20 20:03:23 +1000

    CHange localhost to docker container name in url

--- plateducate-be/app/objectDetection.py
+++ plateducate-be/app/objectDetection.py
@@ -1,13 +1,14 @@
 from flask import Blueprint, request, jsonify
-import requests
+import requests, http.client
 
+http.client._MAXLINE = 655360
 objectDetection = Blueprint('objectDetection', __name__)
 
 @objectDetection.route('/submit_photo', methods=['POST', 'GET'])
 def submit_photo():
     if request.method == 'POST':
         base64Image = request.data
-        url = 'http://localhost:5000/receive_photo'
+        url = 'http://plateducate-ml-server:5000/receive_photo'
         response = requests.post(url, data=base64Image)
 
         if response:
